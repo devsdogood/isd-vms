2842952129315861a9c8f902711cf13d6a659ae3
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-09-09 09:10:33 -0500

    Add event list

---
+++ src/components/product/EventCard.js
@@ -0,0 +1,122 @@
+import PropTypes from 'prop-types';
+import {
+  Box,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography
+} from '@material-ui/core';
+import {
+  Users as UsersIcon,
+  MapPin as MapPinIcon
+} from 'react-feather';
+
+const EventCard = ({ event, ...rest }) => {
+  const dateOptions = {
+    day: 'numeric',
+    month: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  };
+
+  const end = event.end.toDate().toLocaleString([], dateOptions);
+  const start = event.start.toDate().toLocaleString([], dateOptions);
+
+  return (
+    <Card
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%'
+      }}
+      {...rest}
+    >
+      <CardContent>
+        <Typography
+          align="center"
+          color="textPrimary"
+          gutterBottom
+          variant="h4"
+        >
+          {event.title}
+        </Typography>
+        <Typography
+          align="left"
+          color="textPrimary"
+          variant="body1"
+          gutterBottom
+        >
+          {event.description}
+        </Typography>
+        <Typography
+          align="left"
+          color="textSecondary"
+          variant="body1"
+        >
+          {start}
+          {' '}
+          to
+          {' '}
+          {end}
+        </Typography>
+      </CardContent>
+      <Box sx={{ flexGrow: 1 }} />
+      <Divider />
+      <Box sx={{ p: 2 }}>
+        <Grid
+          container
+          spacing={2}
+          sx={{ justifyContent: 'space-between' }}
+        >
+          <Grid
+            item
+            sx={{
+              alignItems: 'center',
+              display: 'flex'
+            }}
+          >
+            <MapPinIcon />
+            <Typography
+              color="textSecondary"
+              display="inline"
+              sx={{ pl: 1 }}
+              variant="body2"
+            >
+              {event.locationName}
+              {' '}
+              at
+              {' '}
+              {event.locationAddress}
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            sx={{
+              alignItems: 'center',
+              display: 'flex'
+            }}
+          >
+            <UsersIcon />
+            <Typography
+              color="textSecondary"
+              display="inline"
+              sx={{ pl: 1 }}
+              variant="body2"
+            >
+              {event.slots}
+              {' '}
+              Slots
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </Card>
+  );
+};
+
+EventCard.propTypes = {
+  event: PropTypes.object.isRequired
+};
+
+export default EventCard;
--- src/pages/EventList.js
+++ src/pages/EventList.js
@@ -1,62 +1,56 @@
+import { useContext } from 'react';
 import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
 import {
   Box,
   Container,
-  Grid,
-  Pagination
+  Grid
 } from '@material-ui/core';
+import { DashboardContext } from 'src/components/DashboardLayout';
 import ProductListToolbar from '../components/product/ProductListToolbar';
-import ProductCard from '../components/product/ProductCard';
-import products from '../__mocks__/products';
+import EventCard from '../components/product/EventCard';
 
-const ProductList = () => (
-  <>
-    <Helmet>
-      <title>Products | Material Kit</title>
-    </Helmet>
-    <Box
-      sx={{
-        backgroundColor: 'background.default',
-        minHeight: '100%',
-        py: 3
-      }}
-    >
-      <Container maxWidth={false}>
-        <ProductListToolbar />
-        <Box sx={{ pt: 3 }}>
-          <Grid
-            container
-            spacing={3}
-          >
-            {products.map((product) => (
-              <Grid
-                item
-                key={product.id}
-                lg={4}
-                md={6}
-                xs={12}
-              >
-                <ProductCard product={product} />
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            pt: 3
-          }}
-        >
-          <Pagination
-            color="primary"
-            count={3}
-            size="small"
-          />
-        </Box>
-      </Container>
-    </Box>
-  </>
-);
+const ProductList = () => {
+  const { events } = useContext(DashboardContext);
+
+  return (
+    <>
+      <Helmet>
+        <title>Events | Material Kit</title>
+      </Helmet>
+      <Box
+        sx={{
+          backgroundColor: 'background.default',
+          minHeight: '100%',
+          py: 3
+        }}
+      >
+        <Container maxWidth={false}>
+          <ProductListToolbar />
+          <Box sx={{ pt: 3 }}>
+            <Grid
+              container
+              spacing={3}
+            >
+              {events.map((event) => (
+                <Grid
+                  item
+                  key={event.id}
+                  lg={4}
+                  md={6}
+                  xs={12}
+                >
+                  <Link to={`/app/events/${event.eventID}`}>
+                    <EventCard event={event} />
+                  </Link>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Container>
+      </Box>
+    </>
+  );
+};
 
 export default ProductList;
