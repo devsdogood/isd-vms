909dc355b951bf0eb288e8f9bd56f73c9305a381
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-10-04 11:13:25 -0500

    Add account profile editing

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,49 +1,61 @@
+import moment from 'moment';
 import { Helmet } from 'react-helmet';
 import {
   Box,
   Container,
   Grid
 } from '@material-ui/core';
+import { useContext } from 'react';
+import { DashboardContext } from 'src/components/DashboardLayout';
 import AccountProfile from '../components/account/AccountProfile';
 import AccountProfileDetails from '../components/account/AccountProfileDetails';
 
-const Account = () => (
-  <>
-    <Helmet>
-      <title>Account | Material Kit</title>
-    </Helmet>
-    <Box
-      sx={{
-        backgroundColor: 'background.default',
-        minHeight: '100%',
-        py: 3
-      }}
-    >
-      <Container maxWidth="lg">
-        <Grid
-          container
-          spacing={3}
-        >
+const Account = () => {
+  const { userData: user } = useContext(DashboardContext);
+  const fullName = [user.firstName, user.lastName].join(' ');
+
+  return (
+    <>
+      <Helmet>
+        <title>Account | Material Kit</title>
+      </Helmet>
+      <Box
+        sx={{
+          backgroundColor: 'background.default',
+          minHeight: '100%',
+          py: 3
+        }}
+      >
+        <Container maxWidth="lg">
           <Grid
-            item
-            lg={4}
-            md={6}
-            xs={12}
+            container
+            spacing={3}
           >
-            <AccountProfile />
+            <Grid
+              item
+              lg={4}
+              md={6}
+              xs={12}
+            >
+              <AccountProfile user={user} fullName={fullName} />
+            </Grid>
+            <Grid
+              item
+              lg={8}
+              md={6}
+              xs={12}
+            >
+              <AccountProfileDetails user={{
+                ...user,
+                birthday: moment(user.birthday.toDate()).format('yyyy-MM-DD')
+              }}
+              />
+            </Grid>
           </Grid>
-          <Grid
-            item
-            lg={8}
-            md={6}
-            xs={12}
-          >
-            <AccountProfileDetails />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  </>
-);
+        </Container>
+      </Box>
+    </>
+  );
+};
 
 export default Account;
