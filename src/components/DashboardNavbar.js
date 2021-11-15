812af8cea2b9aa6779a600cf8a1a61d95d4237cb
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-11-15 15:50:34 -0600

    Remove notification button and support signout

--- src/components/DashboardNavbar.js
+++ src/components/DashboardNavbar.js
@@ -1,21 +1,22 @@
-import { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {
   AppBar,
-  Badge,
   Box,
   Hidden,
   IconButton,
   Toolbar
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
 import InputIcon from '@material-ui/icons/Input';
+import { firebase } from 'src/App';
 import Logo from './Logo';
 
 const DashboardNavbar = ({ onMobileNavOpen, ...rest }) => {
-  const [notifications] = useState([]);
+  // const [notifications] = useState([]);
+  const signOut = () => {
+    firebase.auth().signOut();
+  };
 
   return (
     <AppBar
@@ -28,7 +29,7 @@ const DashboardNavbar = ({ onMobileNavOpen, ...rest }) => {
         </RouterLink>
         <Box sx={{ flexGrow: 1 }} />
         <Hidden xlDown>
-          <IconButton color="inherit" size="large">
+          {/* <IconButton color="inherit" size="large">
             <Badge
               badgeContent={notifications.length}
               color="primary"
@@ -36,8 +37,8 @@ const DashboardNavbar = ({ onMobileNavOpen, ...rest }) => {
             >
               <NotificationsIcon />
             </Badge>
-          </IconButton>
-          <IconButton color="inherit" size="large">
+          </IconButton> */}
+          <IconButton color="inherit" size="large" onClick={signOut}>
             <InputIcon />
           </IconButton>
         </Hidden>
