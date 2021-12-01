023eff1d35cae618cc9f25344c4c84c9076aadc1
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-12-01 08:18:19 -0600

    Add event report generation

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,28 +1,40 @@
 import { Helmet } from 'react-helmet';
-import { Box, Container } from '@material-ui/core';
-import SettingsNotifications from '../components/settings/SettingsNotifications';
-import SettingsPassword from '../components/settings/SettingsPassword';
+import { Box, Button } from '@material-ui/core';
+import { CSVLink } from 'react-csv';
+import { useContext } from 'react';
+import { DashboardContext } from 'src/components/DashboardLayout';
 
-const SettingsView = () => (
-  <>
-    <Helmet>
-      <title>Settings | Iowa Service Dogs VMS</title>
-    </Helmet>
-    <Box
-      sx={{
-        backgroundColor: 'background.default',
-        minHeight: '100%',
-        py: 3
-      }}
-    >
-      <Container maxWidth="lg">
-        <SettingsNotifications />
-        <Box sx={{ pt: 3 }}>
-          <SettingsPassword />
-        </Box>
-      </Container>
-    </Box>
-  </>
-);
+const SettingsView = () => {
+  const {
+    eventSignups, roles, events, users
+  } = useContext(DashboardContext);
+  const filteredSignups = eventSignups.filter((signup) => !signup.deleted);
+
+  const signups = filteredSignups.map((signup) => ({
+    ...signup,
+    role: roles.find((role) => role.roleID === signup.role).title,
+    volunteer: users.find((user) => user.userID === signup.volunteer).firstName,
+    event: events.find((event) => event.eventID === signup.event).title,
+  }));
+
+  return (
+    <>
+      <Helmet>
+        <title>Settings | Iowa Service Dogs VMS</title>
+      </Helmet>
+      <Box
+        sx={{
+          backgroundColor: 'background.default',
+          minHeight: '100%',
+          py: 3
+        }}
+      >
+        <CSVLink data={signups}>
+          <Button>Generate Event Reports</Button>
+        </CSVLink>
+      </Box>
+    </>
+  );
+};
 
 export default SettingsView;
